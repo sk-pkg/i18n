2697cae6dfc98ebf2ac032f36883613265ee6eb1
seakee <seakee23@gmail.com>
2024-06-07 20:22:06 +0800

    优化测试用例

--- i18n_test.go
+++ i18n_test.go
@@ -14,30 +14,60 @@ import (
 
 func TestI18n(t *testing.T) {
 	r := setupRouter()
-	go r.Run(":8080")
+
+	// 创建一个测试服务器。
+	ts := httptest.NewServer(r)
+	defer ts.Close()
 
 	host := "http://127.0.0.1:8080"
 	apis := []result{
-		{Code: -1, Msg: "系统繁忙", Data: "busy", Trace: "busy... "},
-		{Code: 0, Msg: "ok", Data: "ok", Trace: ""},
-		{Code: 500, Msg: "fail", Data: "fail", Trace: ""},
-		{Code: 400, Msg: "请求参数错误", Data: "params", Trace: ""},
-		{Code: 1000, Msg: "你好,Seakee!你的账号是:18888888888", Data: "test", Trace: ""},
+		{Code: -1, Msg: "系统繁忙", Data: "busy", Trace: trace{
+			ID:   "",
+			Desc: "busy... ",
+		}},
+		{Code: 0, Msg: "ok", Data: "ok", Trace: trace{
+			ID:   "1213gsgdfd",
+			Desc: "",
+		}},
+		{Code: 500, Msg: "fail", Data: "fail", Trace: trace{
+			ID:   "",
+			Desc: "",
+		}},
+		{Code: 400, Msg: "Request parameter error", Data: "params", Trace: trace{
+			ID:   "",
+			Desc: "",
+		}},
+		{Code: 1000, Msg: "你好,Seakee!你的账号是:18888888888", Data: "test", Trace: trace{
+			ID:   "",
+			Desc: "",
+		}},
 	}
 
 	for _, api := range apis {
+		req, err := http.NewRequest("GET", host+"/"+api.Data.(string), nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if api.Code == 400 {
+			// 在请求 Header 中设置语言。
+			req.Header.Set("lang", "en-US")
+		}
+
+		// 发送请求并获取响应。
 		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("GET", host+"/"+api.Data.(string), nil)
 		r.ServeHTTP(w, req)
 
-		rs := result{}
-		err := json.Unmarshal(w.Body.Bytes(), &rs)
-		if err != nil {
-			t.Error(err)
+		resp := w.Result()
+		resp.Body.Close()
+
+		var res result
+		if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
+			t.Fatal(err)
 		}
 
 		assert.Equal(t, 200, w.Code)
-		assert.Equal(t, api, rs)
+		assert.Equal(t, api, res)
 	}
 }
 
@@ -53,6 +83,7 @@ func setupRouter() *gin.Engine {
 	})
 
 	r.GET("/ok", func(c *gin.Context) {
+		c.Set("trace_id", "1213gsgdfd")
 		msg.JSON(c, 0, "ok", nil)
 	})
 
